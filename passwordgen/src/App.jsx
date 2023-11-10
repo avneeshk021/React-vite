5007145ae5159cfc5d030ff8c4089b04df05d630
Avneesh Kumar <kaviikumar010@gmail.com>
2023-11-10 14:53:27 +0530

    new

--- passwordgen/src/App.jsx
+++ passwordgen/src/App.jsx
@@ -1,14 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [length, setLength] = useState(8)
+const [numberAllowed , setNumberAllowed] =useState(false);
+const [charAllowed , setcharAllowed] =useState(false);
+const [password , setPassword] =useState("")
 
+const passwordGenerater = useCallback(()=>{
+  let pass=""
+let str ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "
+if(numberAllowed)
+str+="0123456789"
+if(charAllowed)
+str+="!@#$%&*"
+for (let i=1; i<=Array.length; i++)
+{
+  let char = Math.random()* str.length+1
+  pass=str.charAt(char)
+}
+setPassword(pass)
+}, [length, numberAllowed , charAllowed, setPassword])
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generater</h1>
+  
+     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700 text-center'>
+      <h1 className='text-white text-center'>Password Fetcher</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-5'>
+        <input type="text"
+        value={password}
+        className='outline-none w-full py-3 px-4 mb-2'
+        placeholder='Pasword'
+        readOnly
+
+         />
+       
+      </div>
+     </div>
+    
     </>
   )
 }
