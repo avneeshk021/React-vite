d2fa4f3c15c54d3e7995034519e872e1c348b8da
Avneesh Kumar <kaviikumar010@gmail.com>
2023-11-13 20:47:47 +0530

    new

--- currency/src/components/InputBox.jsx
+++ currency/src/components/InputBox.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-
-
+import React, {useId} from 'react'
 function InputBox({
   label,
   amount,
   onAmountChange,
   onCurrencyChnage,
-  currencyOption=[],
-  selectCurrency="usd",
+  currencyOption= [],
+  selectCurrency= "usd",
   amountDisable = false,
-  currencyDisable =false,
+  currencyDisable = false,
   className = "",
 }) {
- 
+ const amountInputId =useId()
 
   return (
       <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
   
           <div className="w-1/2">
-              <label  className="text-black/40 mb-2 inline-block">
+              <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
                   {label}
               </label>
               <input
-                  
+                  id={amountInputId}
                   className="outline-none w-full bg-transparent py-1.5"
                   type="number"
                   placeholder="Amount"
@@ -40,10 +38,15 @@ function InputBox({
                   disabled={currencyDisable}
                   
               >
+                {currencyOption.map((currency)=>(
+                  <option key={currency} value={currency}>
+                 {currency}
+              </option>
+                ))}
                   
-                      <option value="usd">
+                      {/* <option value="usd">
                           usd
-                      </option>
+                      </option> */}
               
               </select>
           </div>
