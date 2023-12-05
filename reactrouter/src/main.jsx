351985e515dee13f3baae4465148017f08981690
Avneesh Kumar <kaviikumar010@gmail.com>
2023-12-05 21:03:23 +0530

    new

--- reactrouter/src/main.jsx
+++ reactrouter/src/main.jsx
@@ -7,6 +7,7 @@ import Layout from './Layout.jsx'
 import Home from './components/Header/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
 // const router = createBrowserRouter([
 //   {
 //     path: '/',
@@ -31,8 +32,9 @@ import Contact from './components/Contact/Contact.jsx'
   createRoutesFromElements(
     <Route path="/" element={<Layout/>}>
       <Route path='' element={<Home/>}/>
-      <Route path='' element={<About/>}/>
-      <Route path='' element={<Contact/>}/>
+      <Route path='about' element={<About/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='user/:userid' element={<User/>}/>
 
     </Route>
   )
