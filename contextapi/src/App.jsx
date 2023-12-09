5525220444139c959047e3e9fae4d4868218ad3b
Avneesh Kumar <kaviikumar010@gmail.com>
2023-12-09 15:06:19 +0530

    new

--- contextapi/src/App.jsx
+++ contextapi/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import UserContext from './context/UserContext'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <h1>React with Chai</h1>
-    </>
+    <UserContextProvider>
+      <h1></h1>
+    </UserContextProvider>
   )
 }
 
