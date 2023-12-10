809f3a35f4b772b8936c790480d020c8d8d43658
Avneesh Kumar <kaviikumar010@gmail.com>
2023-12-10 14:06:03 +0530

    Added my project

--- contextapi/src/App.jsx
+++ contextapi/src/App.jsx
@@ -1,8 +1,7 @@
 
 import './App.css'
-import Login from './Components/Login'
-import Profile from './Components/Profile'
-import UserContext from './context/UserContext'
+import Login from './components/Login'
+import Profile from './components/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
 function App() {
@@ -10,9 +9,9 @@ function App() {
 
   return (
     <UserContextProvider>
-      <h1>React with chai</h1>
-     <Login/>
-      <Profile/>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   )
 }
--- contextapi/src/context/UserContextProvider.jsx
+++ contextapi/src/context/UserContextProvider.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import UserContext from "./UserContext";
- const UserContextProvider =( {Children}) =>
- {
-    const [user, setUser ] = React.useState(null)
-return(
-    <UserContext.Provider value={{user,setUser}}>
-    {Children}
-    </UserContext.Provider>
-)
- }
- export default UserContextProvider
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = React.useState(null)
+    return(
+        <UserContext.Provider value={{user, setUser}}>
+        {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider
