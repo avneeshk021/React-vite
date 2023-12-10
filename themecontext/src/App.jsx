b5534baab8b3eb3e150ad3468d95eb2737f37737
Avneesh Kumar <kaviikumar010@gmail.com>
2023-12-10 14:31:06 +0530

    new

--- themecontext/src/App.jsx
+++ themecontext/src/App.jsx
@@ -2,22 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeProvider } from './contexts/Theme'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
     <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            
+          {/* theme button */}
         </div>
 
         <div className="w-full max-w-sm mx-auto">
-           
+           {/* Card */}
         </div>
     </div>
 </div>
+</ThemeProvider>
   )
 }
 
