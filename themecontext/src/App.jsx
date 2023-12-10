cc586575df5405bac248597679e4fc072f6939bb
Avneesh Kumar <kaviikumar010@gmail.com>
2023-12-10 14:17:15 +0530

    new

--- themecontext/src/App.jsx
+++ themecontext/src/App.jsx
@@ -7,9 +7,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='p-4 bg-pink-600 text-3xl'>Hello React Vite App</h1>
-    </>
+    <div className="flex flex-wrap min-h-screen items-center">
+    <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+           
+        </div>
+    </div>
+</div>
   )
 }
 
