31d063df94783580c99476cb6afc374e114cdf37
Avneesh Kumar <kaviikumar010@gmail.com>
2023-12-11 19:43:29 +0530

    new

--- themecontext/src/App.jsx
+++ themecontext/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { ThemeProvider } from './contexts/Theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
   const [themeMode,setThemeMode] =useState("light")
@@ -16,7 +18,7 @@ function App() {
   useEffect(()=>{
    document.querySelector('html').classList.remove("light","dark")
    document.querySelector('html').classList.add("themeMode")
-  },[]
+  },[themeMode]
   )
 
   return (
@@ -24,11 +26,11 @@ function App() {
     <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          {/* theme button */}
+        <ThemeBtn/>
         </div>
 
         <div className="w-full max-w-sm mx-auto">
-           {/* Card */}
+           <Card/>
         </div>
     </div>
 </div>
